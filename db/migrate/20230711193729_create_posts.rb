2bd57342b9ee615bb07b3532bca2d8d2df1cc5ad
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-11 19:45:13 +0000

    Generate Posts table

--- db/migrate/20230711193729_create_posts.rb
+++ db/migrate/20230711193729_create_posts.rb
@@ -1,8 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.integer :author_id
+      t.string :title
+      t.text :text
       t.timestamps
+      t.integer :comments_counter
+      t.integer :likes_counter
     end
+    add_foreign_key :posts, :users, column: :author_id
+    add_index :posts, :author_id
   end
 end
