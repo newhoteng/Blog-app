2802526241d84eecc1666cb400fe841f570a1620
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-13 15:20:04 +0000

    Add method that updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # updates the comments_counter for a post
+  scope :update_comments_counter, lambda { |post_id|
+                                    comments_for_post = where('post_id = ?', post_id)
+                                    total_comments = comments_for_post.count
+                                    post = comments_for_post.first.post
+                                    post.update(comments_counter: total_comments)
+                                  }
 end
