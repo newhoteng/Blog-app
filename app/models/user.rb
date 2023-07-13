41acd1cfcfe13c0aa154dc47dff4c9ac447505d2
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-13 12:08:33 +0000

    Insert data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: "author_id"
+  has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  # Returns the 3 most recent posts for a given user
+  scope :recent_three_posts, lambda { |user_id|
+                               joins(:posts).where('users.id = ?', user_id).order('posts.created_at DESC').limit(3)
+                             }
 end
