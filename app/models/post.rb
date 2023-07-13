0b3e0c78731ab76776cc365004ca2ebb1ecb5d80
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-13 15:02:21 +0000

    Add method which returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,14 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  # Updates the posts counter for a user
   scope :update_posts_counter, lambda { |user_id|
                                  posts_for_user = where('author_id = ?', user_id)
                                  total_posts = posts_for_user.count
                                  user = posts_for_user.first.user
                                  user.update(posts_counter: total_posts)
                                }
+
+  # Returns the 5 most recent comments for a given post
+  scope :recent_five_comments, ->(post_id) { find(post_id).comments.order('comments.created_at DESC').limit(5) }
 end
