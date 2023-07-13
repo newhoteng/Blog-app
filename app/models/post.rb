7bac54f034f71f8d9a0b3c7ab07948088e4e3d14
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-13 14:48:45 +0000

    Add method to update posts_counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  scope :update_posts_counter, lambda { |user_id|
+                                 posts_for_user = where('author_id = ?', user_id)
+                                 total_posts = posts_for_user.count
+                                 user = posts_for_user.first.user
+                                 user.update(posts_counter: total_posts)
+                               }
 end
