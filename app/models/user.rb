3f71d628e1881a2e5d4e11c4afa8436631bf9f9c
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-14 10:07:19 +0000

    Add validations to User and Post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+  
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   # Returns the 3 most recent posts for a given user
   def recent_three_posts
     posts.order('created_at Desc').limit(3)
   end
 end
+
+# harriet = User.find(5)
+# harriet.recent_three_posts
