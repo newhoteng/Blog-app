c42efeb41b2c5292bc6be1dcbc591078ffb17d35
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-18 21:33:12 +0000

    Add tests for Posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users'
+      get '/users/user_id/posts'
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
     it 'renders the correct template' do
-      get '/users'
+      get '/users/user_id/posts'
       expect(response).to render_template('index')
     end
     it 'response body includes placeholder' do
-      get '/users'
-      expect(response.body).to include('Here is a list of all users')
+      get '/users/user_id/posts'
+      expect(response.body).to include('Here are posts of a specific user')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/user_id'
+      get '/users/user_id/posts/post_id'
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
     it 'renders the correct template' do
-      get '/users/user_id'
+      get '/users/user_id/posts/post_id'
       expect(response).to render_template('show')
     end
     it 'response body includes placeholder' do
-      get '/users/user_id'
-      expect(response.body).to include('Here are details of a specific user')
+      get '/users/user_id/posts/post_id'
+      expect(response.body).to include('Here are details of a specific post for a specific user')
     end
   end
 end
