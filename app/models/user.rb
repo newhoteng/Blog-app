08d0c11b1c6d0f7cc6c1b20493617607f14387f9
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-19 18:02:17 +0000

    Display all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes
-  has_many :comments
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
