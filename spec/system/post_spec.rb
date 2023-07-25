1d1c37023c61a2a6be21fb387c515f298e5e1fd9
Naji-97 <elbairabennaji@gmail.com>
2023-07-25 09:20:07 +0100

    setup test for post

---
+++ spec/system/post_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Post', type: :feature do
+  # user_post GET    /users/:user_id/posts/:id(.:format)  posts#show
+  # user_posts GET    /users/:user_id/posts(.:format)     posts#index
+
+  # CONTENTS
+
+end
