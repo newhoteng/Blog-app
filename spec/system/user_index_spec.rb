4ff74402db07e4c5a644adbd774676445d9dfaaf
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-25 05:41:45 +0000

    Add tests for user index and show pages

--- spec/system/user_index_spec.rb
+++ spec/system/user_index_spec.rb
@@ -1,31 +1,78 @@
 require 'rails_helper'
 
-RSpec.describe 'User views', type: :feature do
-  describe 'content of index page' do
-    user = User.first
-
+RSpec.describe 'User', type: :feature do
+  describe "index page's content and features" do
+    users = User.all
     before :each do
       visit users_path
     end
-    it "should show the user's name" do
-      expect(page).to have_content(user.name)
+    describe "page's content" do
+      it "should show the user's name" do
+        users.each { |user| expect(page).to have_content user.name }
+      end
+
+      it "should show the user's pic" do
+        users.each { |user| expect(page).to have_css("img[src='#{user.photo}']") }
+      end
+
+      it 'should display the number of posts' do
+        users.each { |user| expect(page).to have_content("Number of posts: #{user.posts.count}") }
+      end
+    end
+
+    describe 'clicking on user link feature' do
+      it 'redirects to show page' do
+        @user = User.first
+        click_link(:href => user_path(@user))
+        expect(page).to have_current_path(user_path(@user))
+      end
     end
   end
 
-  # describe '#update_likes_counter method' do
-  #   it 'it increments the likes_counter of a post' do
-  #     user = User.create(name: 'Doe')
-  #     post = Post.create(author_id: user.id, title: 'Hello', text: 'This is my first post')
+  describe "show page's content and features" do
+    user = User.first
+    recent_posts = user.recent_three_posts
+    before :each do
+      visit user_path(user)
+    end
+    describe "page's content" do
+      it "should show the user's pic" do
+        expect(page).to have_css("img[src='#{user.photo}']") 
+      end
 
-  #     expect(post.likes_counter).to eql 0
+      it "should show the user's name" do
+        expect(page).to have_content user.name
+      end
 
-  #     Like.create(user_id: user.id, post_id: post.id)
-  #     post.reload
-  #     expect(post.likes_counter).to eql 1
+      it 'should display the number of posts' do
+        expect(page).to have_content("Number of posts: #{user.posts.count}")
+      end
 
-  #     Like.create(user_id: user.id, post_id: post.id)
-  #     post.reload
-  #     expect(post.likes_counter).to eql 2
-  #   end
-  # end
+      it "should show the user's bio" do
+        expect(page).to have_content("#{user.bio}")
+      end
+
+      it "should show the user's three posts" do
+        recent_posts.each { |post| expect(page).to have_content("#{post.text}") }
+      end
+
+      it "should see a link to 'See all posts'" do
+        expect(page).to have_link('See all posts')
+      end
+    end
+
+    describe 'interactions on page' do
+      it "redirects to post's show page when post is clicked" do
+        recent_posts.each do |post|
+          click_link(:href => user_post_path(user, post))
+          expect(page).to have_current_path(user_post_path(user, post))
+        end
+      end
+
+      it "redirects to post's index page when 'See all post' clicked" do
+        click_link('See all post')
+        expect(page).to have_current_path(user_posts_path(user))
+      end
+    end
+  end
 end
