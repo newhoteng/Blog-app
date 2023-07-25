3131d27ab63b8a28ea0f72fe9aedc7dcb1f759e5
Naji-97 <elbairabennaji@gmail.com>
2023-07-25 09:22:53 +0100

    describe content tes

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -4,6 +4,18 @@ RSpec.describe 'Post', type: :feature do
   # user_post GET    /users/:user_id/posts/:id(.:format)  posts#show
   # user_posts GET    /users/:user_id/posts(.:format)     posts#index
 
-  # CONTENTS
+  describe "index page's content and features" do
+    user = User.first
+    user_posts = user.posts
+    before :each do
+      visit user_posts_path(user)
+    end
+    describe "page's content" do
+      #contents
+    end
 
+    describe 'clicking on post' do
+      #contents
+    end
+  end
 end
