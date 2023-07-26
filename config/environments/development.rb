cf115faf1cac837b8ab67a19e6eccb05ad350cab
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-26 21:48:06 +0000

    Set up google smtp server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
+  ######
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.credentials.gmail_username,
+    password:       Rails.application.credentials.gmail_password,
+    domain:        'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  #####
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
